f1edf27f7321926ef86f23b71c95ab2b3b964f4f
Ashish Shukla <ashishfun008@gmail.com>
2022-02-04 23:59:51 +0530

    added api docs for object response

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
 app.get("/api/v1/users", (req, res) => {
   res.send("users");
 });
@@ -22,10 +21,38 @@ app.get("/api/v1/products", (req, res) => {
     name: "A book",
     id: "2343",
     author: "authero anem",
-    publishedData: new Date()
+    addedDate: new Date(),
+    price: 433,
   });
 });
 
+const coursesData = [
+  {
+    id: "34js",
+    name: "JS Course",
+    description: "A Complete JavaScript course",
+    price: 456,
+    effortHour: 100,
+  },
+  {
+    id: "56py",
+    name: "Python Course",
+    description: "A Complete Python course",
+    price: 200,
+    effortHour: 60,
+  },
+  {
+    id: "90C",
+    name: "C Course",
+    description: "A Complete C course",
+    price: 1000,
+    effortHour: 2000,
+  },
+];
+
+app.get("/api/v1/courses", (req, res) => {
+  return res.status(200).json(coursesData);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
